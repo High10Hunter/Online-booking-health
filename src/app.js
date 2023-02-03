ed9656063ee2a1116e1776eec0863e08349ffe19
Tran Dai Quy <trandaiquy2903@gmail.com>
2023-02-04 00:29:45 +0700

    add cookie-parser middleware

--- src/app.js
+++ src/app.js
@@ -1,18 +1,27 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+
 import web from './api/routes';
 import viewEngine from './api/config/viewEngine';
 
 import notFoundMiddleware from './api/middlewares/not-found';
+import errorHandlerMiddleware from './api/middlewares/error-handler';
+import issueAuthHeader from './api/middlewares/issueAuthHeader';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 viewEngine(app);
 
+app.use(issueAuthHeader);
+
+// web.testRoute(app);
 web.authRoutes(app);
 web.adminRoutes(app);
 
 app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 export default app;
