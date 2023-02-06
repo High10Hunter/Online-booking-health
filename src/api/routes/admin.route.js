530df994796cbc98a73d7bb0d51bfb4c646ae310
Tran Dai Quy <trandaiquy2903@gmail.com>
2023-02-06 21:44:15 +0700

    update (route & UI - admin) : index

--- src/api/routes/admin.route.js
+++ src/api/routes/admin.route.js
@@ -1,16 +1,41 @@
 import { Router } from 'express';
 import UserController from '../controllers/admin/UserController';
+import RolesEnum from '../enums/RolesEnum';
+import verifyRoles from '../middlewares/verifyRoles';
+import { verifyAccessToken } from '../services/jwt/JwtServices';
 
 const router = Router();
 
+// const adminRoutes = app => {
+// 	//manage users routes
+// 	router.get('/users', UserController.index);
+// 	router.post('/users/create', UserController.create);
+// 	router.patch('/users/update/:id', UserController.update);
+// 	router.delete('/users/destroy/:id', UserController.destroy);
+
+// 	app.use('/api/admin', router);
+// };
+
 const adminRoutes = app => {
 	//manage users routes
+	router.get(
+		'/',
+		verifyAccessToken,
+		verifyRoles(RolesEnum.ADMIN),
+		(req, res) => {
+			res.render('admin/index', {
+				title: 'Admin',
+			});
+		}
+	);
+
+	//CRUD users
 	router.get('/users', UserController.index);
 	router.post('/users/create', UserController.create);
 	router.patch('/users/update/:id', UserController.update);
 	router.delete('/users/destroy/:id', UserController.destroy);
 
-	app.use('/api/admin', router);
+	app.use('/admin', router);
 };
 
 export default adminRoutes;
