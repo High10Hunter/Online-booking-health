766d2ce46ea3b9e514b836660d20ed94e50712c6
Thanh <ldthanh2001@gmail.com>
2023-03-22 23:08:10 +0700

    search_by_specialty, form_Appointment

--- src/api/routes/client.route.js
+++ src/api/routes/client.route.js
@@ -1,15 +1,21 @@
-import { Router } from 'express';
+import { Router } from "express";
 
 const router = Router();
 
-const clientRoutes = app => {
-	router.get('/', (req, res) => {
-		res.render('./client/index', {
-			layout: './client_layouts/master',
-		});
-	});
+const clientRoutes = (app) => {
+  router.get("/", (req, res) => {
+    res.render("./client/index", {
+      layout: "./client_layouts/master",
+    });
+  });
 
-	app.use('/', router);
+  router.get("/booking", (req, res) => {
+    res.render("./client/booking", {
+      layout: "./client_layouts/master",
+    });
+  });
+
+  app.use("/", router);
 };
 
 export default clientRoutes;
