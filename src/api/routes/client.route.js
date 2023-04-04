8b03d8d055885f42a16d089936d175e3c093b9d0
Thanh <ldthanh2001@gmail.com>
2023-04-04 13:54:19 +0700

    Booking-page, dotor-list, doctor-profile/bac-si-chuyen-khoa-ii-nguyen-thi-kim-loan

--- src/api/routes/client.route.js
+++ src/api/routes/client.route.js
@@ -15,6 +15,18 @@ const clientRoutes = (app) => {
     });
   });
 
+  router.get("/doctor-list", (req, res) => {
+    res.render("./client/doctor_list", {
+      layout: "./client_layouts/master",
+    });
+  });
+
+  router.get("/doctor-profile/bac-si-chuyen-khoa-ii-nguyen-thi-kim-loan", (req, res) => {
+    res.render("./client/doctor_profile", {
+      layout: "./client_layouts/master",
+    });
+  });
+
   app.use("/", router);
 };
 
