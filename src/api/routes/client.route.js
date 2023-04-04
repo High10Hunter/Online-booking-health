91db3ea70ff52ffd7e6a81761fe68436f3e81a67
Tran Dai Quy <trandaiquy2903@gmail.com>
2023-04-04 23:12:45 +0700

    refactor(client - UI)

--- src/api/routes/client.route.js
+++ src/api/routes/client.route.js
@@ -1,33 +1,33 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const router = Router();
 
-const clientRoutes = (app) => {
-  router.get("/", (req, res) => {
-    res.render("./client/index", {
-      layout: "./client_layouts/master",
-    });
-  });
+const clientRoutes = app => {
+	router.get('/', (req, res) => {
+		res.render('./client/index', {
+			layout: './layouts/client_layouts/master',
+		});
+	});
 
-  router.get("/booking", (req, res) => {
-    res.render("./client/booking", {
-      layout: "./client_layouts/master",
-    });
-  });
+	router.get('/booking', (req, res) => {
+		res.render('./client/booking', {
+			layout: './layouts/client_layouts/master',
+		});
+	});
 
-  router.get("/doctor-list", (req, res) => {
-    res.render("./client/doctor_list", {
-      layout: "./client_layouts/master",
-    });
-  });
+	router.get('/doctor-list', (req, res) => {
+		res.render('./client/doctor_list', {
+			layout: './layouts/client_layouts/master',
+		});
+	});
 
-  router.get("/doctor-profile/bac-si-chuyen-khoa-ii-nguyen-thi-kim-loan", (req, res) => {
-    res.render("./client/doctor_profile", {
-      layout: "./client_layouts/master",
-    });
-  });
+	router.get('/doctor-profile', (req, res) => {
+		res.render('./client/doctor_profile', {
+			layout: './layouts/client_layouts/master',
+		});
+	});
 
-  app.use("/", router);
+	app.use('/', router);
 };
 
 export default clientRoutes;
