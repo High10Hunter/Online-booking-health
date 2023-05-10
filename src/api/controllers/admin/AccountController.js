5a716186b2353be061cbc28bf3dfa16be73db512
Tran Dai Quy <trandaiquy2903@gmail.com>
2023-05-10 08:40:55 +0700

    fix (account): display different layout bases on role

--- src/api/controllers/admin/AccountController.js
+++ src/api/controllers/admin/AccountController.js
@@ -1,19 +1,36 @@
 import { StatusCodes } from 'http-status-codes';
 import User from '../../services/admin/UserServices';
 import { configureMulter } from '../../utils';
+import RolesEnum from '../../enums/RolesEnum';
 
 const index = async (req, res) => {
 	try {
-		const { id } = req.payload;
+		const { id, role } = req.payload;
 		const user = await User.getUserById(id);
 
-		return res.render('./admin/account', {
-			title: 'Cài đặt tài khoản',
-			id,
-			user,
-		});
+		if (role === RolesEnum.ADMIN) {
+			return res.render('./admin/account', {
+				title: 'Cài đặt tài khoản',
+				id,
+				user,
+			});
+		} else if (role === RolesEnum.NURSE) {
+			return res.render('./admin/account', {
+				title: 'Cài đặt tài khoản',
+				id,
+				user,
+				layout: './layouts/nurse_layout/master',
+			});
+		} else if (role === RolesEnum.DOCTOR) {
+			return res.render('./admin/account', {
+				title: 'Cài đặt tài khoản',
+				id,
+				user,
+				layout: './layouts/doctor_layout/master',
+			});
+		}
 	} catch (error) {
-		return res.redirect('/admin');
+		return res.redirect('/auth/login');
 	}
 };
 
@@ -33,19 +50,19 @@ const update = async (req, res) => {
 		} else {
 			req.body.avatar = '';
 		}
-		
+
 		try {
-			const {new_password, confirm_password} = req.body;
-	
+			const { new_password, confirm_password } = req.body;
+
 			if (new_password !== confirm_password) {
 				return res.status(StatusCodes.BAD_REQUEST).json({
 					message: 'Password does not match',
 					data: [],
 				});
 			}
-	
+
 			const user = await User.updateUser(req.payload.id, req.body);
-	
+
 			return res.status(StatusCodes.OK).json({
 				message: 'Update user successfully',
 				data: user,
@@ -56,8 +73,7 @@ const update = async (req, res) => {
 				data: [],
 			});
 		}
-	})
-
+	});
 };
 
 export default {
